991ed8f4f1df3ae2e5427bcd8f532b94b6dae867
Marco Neumann <marco@crepererum.net>
2013-04-26 13:03:10 +0200

    First simple console

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+
 int main() {
-	return 0;
+	// print hello message
+	std::cout << "GreyCore" << std::endl;
+
+	// main loop
+	bool exit = false;
+	while (!exit) {
+		// read new command
+		std::string cmd;
+		std::cout << ">> " << std::flush;
+		std::cin >> cmd;
+
+		// parse command
+		if (cmd == "exit") {
+			exit = true;
+		}
+	}
+
+	// cleanup and exit
+	return EXIT_SUCCESS;
 }
 
